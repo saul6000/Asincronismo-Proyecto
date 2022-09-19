1ddf4301d6dbc25f46a92dc0c940a1807d4c4b94
Saul Mendoza <saulmendoza6000@gmail.com>
2022-09-19 18:49:19 -0500

    mostrar los datos de mi pagina

---
+++ src/js/boton.js
@@ -0,0 +1,6 @@
+const Principal = ()=>{
+    window.location = 'index.html';
+      return ;
+};
+const principal = document.querySelector('.Principal');
+principal.addEventListener('click',Principal);
